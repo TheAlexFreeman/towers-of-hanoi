59e015e32d643498faabf5240e94ef066d4b3624
TheAlexFreeman <alexrfreeman@protonmail.com>
2021-02-12 19:51:05 -0600

    Basic logic up to 3 moves

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 
+const TOWERS = [[], [], []];
+
+function canMove(from, to) {
+    const FROM = TOWERS[from];
+    const TO = TOWERS[TO];
+    return FROM.length && (!TO.length || FROM[0] < TO[0]);
+}
+
+function move(from, to) {
+    if (!canMove(from, to)) return;
+    const FROM = TOWERS[from];
+    const TO = TOWERS[TO];
+    const newHeight = TO.unshift(FROM.shift());
+    console.log(`Disk of size ${TO[0]} moved from ${from} to ${to}`);
+}
+
+function move2(from, to) {
+    const temp = 3 - from - to; // ONLY works with 3 towers!
+    if (!canMove(from, temp)) return;
+    move(from, temp);
+    if (!canMove(from, to)) return;
+    move(from, to);
+    move(temp, to);
+}
+
+function move3(from, to) {
+    const temp = 3 - from - to;
+    if (!canMove(from, temp)) return;
+    move2(from, temp);
+    if (!canMove(from, to)) return;
+    move(from, to);
+    move2(temp, to);
+}
